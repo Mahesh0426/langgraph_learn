ef266aee3cc4209222daf286f158fc3812eadf48
Mahesh792 <mawhesjung143@gmail.com>
2025-05-06 13:53:39 +1000

    add MongoDB checkpointer to persist chatbot state using thread_id

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 from langgraph.checkpoint.mongodb import MongoDBSaver
 
 
-MONGODB_URI = "localhost:27017"
+MONGODB_URI = "mongodb://admin:admin@localhost:27017"
+config ={"configurable":{"thread_id":"1"}}
 load_dotenv()
 
 def init():
@@ -13,10 +14,10 @@ def init():
     with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
         graph_with_mongo = create_chat_graph(checkpointer = checkpointer)
 
-    while True:
-        user_input = input("> ")
-        for event in  graph.stream({"messages": [{"role":"user", "content": user_input}] }, stream_mode="values"):
-            if "messages" in event:
-                event["messages"][-1].pretty_print()
+        while True:
+            user_input = input("> ")
+            for event in  graph_with_mongo.stream({"messages": [{"role":"user", "content": user_input}] }, config, stream_mode="values"):
+                if "messages" in event:
+                    event["messages"][-1].pretty_print()
                                 
 init()
