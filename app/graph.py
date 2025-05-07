6f962f6679dc4534cc23c194e5b9b01d2eb0686e
Mahesh792 <mawhesjung143@gmail.com>
2025-05-08 01:05:42 +1000

    feat: add Human-in-the-Loop (HITL) support with interrupt-based tool in LangGraph

--- app/graph.py
+++ app/graph.py
@@ -3,34 +3,63 @@ from typing import Annotated
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.types  import interrupt
+from langgraph.prebuilt import ToolNode, tools_condition
+
+#define a callable tool that can be invoked by the LLM
+@tool
+def human_assistance_tool(query: str):
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})  # Pauses the graph and saves state
+    return human_response["data"]  # Resumes with human-provided data
+
+# Define the tools
+tools = [human_assistance_tool]
+
+
 
 # Initialize the chat model
-llm = ChatOpenAI(
-    model="gpt-4",  
-    temperature=0.7,
-)
+llm = ChatOpenAI(model="gpt-4", temperature=0.7,)
+
+#  Tool Binding
+llm_with_tool = llm.bind_tools(tools=tools)
 
 # Define the state graph Structure
 class State(TypedDict):
-   messages: Annotated[list, add_messages]
+   messages: Annotated[list, add_messages]  # Metadata
 
 #Define the Chatbot Function
 def chatbot (state:State):
-#    messages = state.get("messages")
+#    messages = state.get("messages") # Read the current messages
 #    response = llm.invoke(messages)  #sends the messages to the model to gets a response
-#    return {"messages":[response]}
-    return {"messages": [llm.invoke(state["messages"])]}
+#    return {"messages":[response]}   # Return a new state
+   # return {"messages": [llm.invoke(state["messages"])]}
+      message = llm_with_tool.invoke(state["messages"])  #sends the messages to the model to gets a response
+      assert len(message.tool_calls) <= 1
+      return {"messages":[message]}   
+
+tool_node = ToolNode(tools=tools)
 
 # Initialize the graph with the chatbot state definition
 graph_builder = StateGraph(State)
 
-# Add the chatbot function to the graph
+# Add the Add the Chatbot Node to the graph
 graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("tools", tool_node)
+
+#Define the start and end nodes
 graph_builder.add_edge(START, "chatbot")
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge("chatbot", END)
 
 #without any memory
 graph = graph_builder.compile()
+# graph.interrupt_after_nodes ("confirm_transaction")
 
 #create a new graph with a checkpointer
 def create_chat_graph(checkpointer):
--- app/main.py
+++ app/main.py
@@ -6,9 +6,10 @@ from langgraph.checkpoint.mongodb import MongoDBSaver
 
 
 MONGODB_URI = "mongodb://admin:admin@localhost:27017"
-config ={"configurable":{"thread_id":"1"}}
+config ={"configurable":{"thread_id":"6"}}
 load_dotenv()
 
+#Chat Loop (init Function)
 def init():
     # check pointer
     with MongoDBSaver.from_conn_string(MONGODB_URI) as checkpointer:
@@ -16,6 +17,7 @@ def init():
 
         while True:
             user_input = input("> ")
+            # Streaming the Graph
             for event in  graph_with_mongo.stream({"messages": [{"role":"user", "content": user_input}] }, config, stream_mode="values"):
                 if "messages" in event:
                     event["messages"][-1].pretty_print()
